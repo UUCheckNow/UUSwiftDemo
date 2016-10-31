8d2510845179d0b3a45e65497dfc76976855ee10
lhn <lhn@xiaoheiliangdeimac.local>
2016-10-31 18:37:23 +0800

    Initial Commit

---
+++ UUDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UUDemo
+//
+//  Created by lhn on 16/10/31.
+//  Copyright © 2016年 UUU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
