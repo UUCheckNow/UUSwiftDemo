0869eb63e79f4b7c90084aecb8ec9405d0341c8c
lhn <lhn@xiaoheiliangdeiMac.local>
2016-11-02 19:06:24 +0800

    添加navg和tabbar

---
+++ UUDemo/HOME/HomeCell/HomeTabCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTabCell.swift
+//  UUDemo
+//
+//  Created by lhn on 16/11/2.
+//  Copyright © 2016年 UUU. All rights reserved.
+//
+
+import UIKit
+
+class HomeTabCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
